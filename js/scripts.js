3b76f77d1f3862919587f18f21397cd7325560a1
David Monarrez and ward chamberlain <ward@>
2019-01-15 15:38:15 -0800

    It will have a toggle theme

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
           trigger: 'click'
       });
   });
+$(".theme").click(function() {
+  $('body').toggleClass('dark-theme');
+  $('.col-xs-3').toggleClass('dark-theme');
+  $('.light').toggle();
+  $('.dark').toggle();
+  $('h1').toggleClass('dark-header');
+});
+
 
   $(".back").css("display", "block");
 });
